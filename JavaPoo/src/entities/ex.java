2a47ce6e33a18b072e4ae3eadfa288bfe62e0cf7
Matheus Silva <matheuspinheiro382@gmail.com>
2025-10-29 15:41:59 -0300

    File, FileReader e BufferedReader

---
+++ JavaPoo/src/entities/ex.java
@@ -0,0 +1,26 @@
+package entities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ex {
+    //          File file = new File("C:\\Users\\Dell\\Documents\\text.txt");
+//          //C:\Users\Dell\Documents\text.txt
+//        String patch = "c:\\Users\\Dell\\Documents\\text";
+//
+//
+//
+//       try {
+//           ent = new Scanner(file);
+//            while (ent.hasNextLine()){
+//                System.out.println(ent.nextLine());
+//            }
+//        }
+//        catch (IOException e) {
+//            System.out.println("ERRO " + e.getMessage());
+//        }
+//        finally {
+//            ent.close();
+//        }
+}
